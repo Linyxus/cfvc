46afff0697f80f4f03446bf33e0fd2ca53dbbaed
Yichen Xu <yichen.x@outlook.com>
2020-01-15 21:57:06 +0800

    Add ToJSON instances for VJ datatypes

--- src/Network/VJClient/Types.hs
+++ src/Network/VJClient/Types.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE KindSignatures #-}
 module Network.VJClient.Types
   ( module Network.VJClient.Types
@@ -6,6 +8,8 @@ module Network.VJClient.Types
 import Network.Hequests.Request (evalSession_)
 import Network.Hequests.Types
 import Control.Monad.Except (ExceptT, runExceptT, lift, throwError)
+import Network.VJClient.ToJSON
+import Data.ByteString.Char8 (ByteString, pack)
 
 data VJAction = VJLogin
               | VJCreate
@@ -36,3 +40,58 @@ data VJProblem = VJProblem { vjOj :: String
                            , vjProbNum :: String
                            , vjProbName :: String
                            } deriving (Eq, Show)
+
+data VJAccess = Public
+                | Protected String
+                | Private String
+                deriving (Eq, Show)
+
+data VJContest = VJContest { vjBeginTime :: Word
+                           , vjDesc :: String
+                           , vjDescFmt :: String
+                           , vjLength :: Word
+                           , vjAccess :: VJAccess
+                           , vjShowPeers :: Bool
+                           , vjTitle :: String
+                           , vjProblems :: [VJProblem]
+                           , vjAnnouncement :: String
+                           } deriving (Eq, Show)
+
+instance ToJSON VJProblem where
+  encodeJSON VJProblem{..} = encodeObject $ do
+    "alias" .= ("" :: ByteString)
+    "descId" .= (0 :: Int)
+    "oj" .= pack vjOj
+    "pid" .= vjPid
+    "probNum" .= pack vjProbNum
+    "weight" .= (1 :: Int)
+
+instance ToJSON VJContest where
+  encodeJSON VJContest{..} = encodeObject $ do
+    "announcement" .= pack vjAnnouncement
+    "beginTime" .= vjBeginTime
+    "contestId" .= (0 :: Int)
+    "description" .= do
+      "format" .= pack vjDescFmt
+      "content" .= pack vjDesc
+    "groups" .= emptyObject
+    "length" .= vjLength
+    encodeAccess vjAccess
+    "partialScore" .= False
+    "penalty" .= (1200 :: Int)
+    "problems" .= vjProblems
+    "rankCellMeaning" .= JNull
+    "showPeers" .= vjShowPeers
+    "sumTime" .= True
+    "timeMachine" .= True
+    "type" .= (0 :: Int)
+    "title" .= pack vjTitle
+
+encodeAccess :: VJAccess -> EncodeObject ()
+encodeAccess Public = "openness" .= (0 :: Int)
+encodeAccess (Protected p) = do
+  "openness" .= (1 :: Int)
+  "password" .= pack p
+encodeAccess (Private p) = do
+  "openness" .= (2 :: Int)
+  "password" .= pack p
